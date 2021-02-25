ff1e663b78c247690d5d7c02c97f61ae5a771670
[kelvo1999] <[kelvoshisanya@gmail.com]>
2021-02-25 15:26:56 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#simple sum calculator
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+c =  int (a) +  int (b)
+print("The sum is")
+print(c )
+
+#simple subtraction
+
+d = input("Enter a number: ")
+e = input("Enter another number: ")
+f = int(d) - int(e)
+print("the difference is:")
+print(f)
+
+#simple modulus
+
+g = input("Enter a number: ")
+h = input("Enter another number: ")
+i = int(g) % int(h)
+print("the mod is :")
+print(f)
